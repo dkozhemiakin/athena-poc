3d7cfa31f76d00fe317840bf27e4af897f627f7e
dkozhemiakin <dmytro.kozhemiakin@gmail.com>
2018-10-05 13:11:55 -0400

    Increased data amount in bucket

--- src/aws/upload.sh
+++ src/aws/upload.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-aws s3 cp ./dataset.csv.gz s3://test-ebsco-athena-poc/data/ --profile eis-deliverydevqa
+aws s3 cp ./dataset.csv.gz s3://test-ebsco-athena-poc/data/dataset1.csv.gz --profile eis-deliverydevqa
+aws s3 cp ./dataset.csv.gz s3://test-ebsco-athena-poc/data/dataset2.csv.gz --profile eis-deliverydevqa
+aws s3 cp ./dataset.csv.gz s3://test-ebsco-athena-poc/data/dataset3.csv.gz --profile eis-deliverydevqa
+aws s3 cp ./dataset.csv.gz s3://test-ebsco-athena-poc/data/dataset4.csv.gz --profile eis-deliverydevqa
+aws s3 cp ./dataset.csv.gz s3://test-ebsco-athena-poc/data/dataset5.csv.gz --profile eis-deliverydevqa
+aws s3 cp ./dataset.csv.gz s3://test-ebsco-athena-poc/data/dataset6.csv.gz --profile eis-deliverydevqa
+aws s3 cp ./dataset.csv.gz s3://test-ebsco-athena-poc/data/dataset7.csv.gz --profile eis-deliverydevqa
+aws s3 cp ./dataset.csv.gz s3://test-ebsco-athena-poc/data/dataset8.csv.gz --profile eis-deliverydevqa
+aws s3 cp ./dataset.csv.gz s3://test-ebsco-athena-poc/data/dataset9.csv.gz --profile eis-deliverydevqa
+aws s3 cp ./dataset.csv.gz s3://test-ebsco-athena-poc/data/dataset10.csv.gz --profile eis-deliverydevqa
